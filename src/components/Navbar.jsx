753e82bd69c20d66b39333329d17b06083d76a61
Shashank Lakhera <lakherashashank70@gmail.com>
2026-03-19 16:42:39 +0530

    Improved UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,24 +26,24 @@ const Navbar = ({ addPost }) => {
     }, [lastScrollY])
 
   return (
-    <div className={`w-full  px-5 md:px-15  py-3 sm:py-1 flex justify-between pt-1 fixed ${isVisible ? 'translate-y-0' : '-translate-y-full'} backdrop-blur-sm`}>
-      <div className='w-full md:w-12 flex  overflow-hidden items-center'>
+    <div className={`w-full  px-5 md:px-15  py-3 sm:py-1 flex justify-between pt-2 fixed ${isVisible ? 'translate-y-0' : '-translate-y-full'} backdrop-blur-sm`}>
+      <div className='w-10  md:w-12 flex  overflow-hidden items-center'>
         <img src={logo} alt="" className=' w-full rounded-4xl' />
       </div>
 
       <div className='w-3/5 flex justify-center mt-1 '>
         <div 
         onClick={()=> addPost()}
-        className='w-full md:w-3/5 lg:w-2/5  flex items-center px-4 rounded-2xl border py-2 text-gray-300 cursor-text  ml-8'
+        className='w-full md:w-3/5 lg:w-2/5  flex items-center pr-1 text-xs sm:text-lg sm:px-4 rounded-2xl border border-gray-400 py-2 text-gray-300 cursor-text  ml-8 shadow-[0_1px_10px_rgb(255,255,255,0.2)] hover:shadow-[0_1px_10px_rgb(255,255,255,0.25)] transition-shadow duration-200'
         >
-          <i className="ri-pencil-fill pr-4"></i>
+          <i className="ri-pencil-fill px-2 sm:pr-4 "></i>
           Create a Post
         </div>
 
 
       </div>
 
-      <div className='flex gap-4 text-2xl items-center '>
+      <div className='flex gap-4 text-md md:text-2xl items-center '>
         <p ><i className="ri-notification-3-line p-1.5 hover:bg-[#111] cursor-pointer rounded-lg"></i></p>
         <p><i className="ri-user-line p-1.5 hover:bg-[#111] rounded-lg cursor-pointer"></i></p>
       </div>
